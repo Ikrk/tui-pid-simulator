404c0d87120c595a93421ac91a80b2472ed3c015
Ikrk <ikrk@centrum.cz>
2025-08-15 02:34:58 +0200

    Wired PID controller

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,7 @@ struct App {
     samples_per_window: usize,
     sampling: f64,
     controller: PIDController,
+    controller_data: Vec<(f64, f64)>,
     simulation_on: bool,
     editing: Editing,
 }
@@ -58,46 +59,132 @@ struct SinSignal {
     amplitude: f64,
 }
 
+#[allow(non_snake_case)]
 #[derive(Clone)]
 struct StepSignal {
     x: f64,
-    interval: f64,
+    Ts: f64,
     amplitude: f64,
     amplitude_edit: Option<NumericInput>,
 }
 
 /// Discrete-time first-order system:
 ///   y_{k+1} = a * y_k + b * u_k
+#[allow(non_snake_case)]
 #[derive(Clone)]
 struct FirstOrderSystem {
     x: f64,
-    interval: f64,
+    Ts: f64,
     a: f64,
     b: f64,
     u: f64,
     y_k: f64,
 }
 
+// Inpired by: https://www.scilab.org/discrete-time-pid-controller-implementation
+#[allow(non_snake_case)]
 #[derive(Clone, Default)]
 struct PIDController {
-    p: f64,
-    i: f64,
-    d: f64,
+    // TODO - antiwindup
+    // TODO - output saturation
+    Kp: f64,             // proportional gain
+    Ki: f64,             // integral gain
+    Kd: f64,             // derivative gain
+    e: (f64, f64, f64),  // (current, previous, before previous) error
+    u: (f64, f64, f64),  // (current, previous, before previous) controller output/plant input
+    y: f64,              // current output of the system
+    r: f64,              // set point (reference input)
+    N: f64,              // derivative filter coefficient
+    Ts: f64,             // sampling time
+    ku: (f64, f64), // difference equation controller output (plant input) u1 and u2 coefficients
+    ke: (f64, f64, f64), // difference equation controller input (error) e0, e1 and e2 coefficients
+    x: f64,         // current time
+}
+
+impl PIDController {
+    #[allow(non_snake_case)]
+    pub fn new(Kp: f64, Ki: f64, Kd: f64, N: f64, Ts: f64) -> Self {
+        let mut pid = Self {
+            Kp,
+            Ki,
+            Kd,
+            e: (0.0, 0.0, 0.0),
+            u: (0.0, 0.0, 0.0),
+            y: 0.0,
+            N,
+            Ts,
+            ku: (1.0, 1.0),
+            ke: (1.0, 1.0, 1.0),
+            x: 0.0,
+            r: 0.0,
+        };
+        pid.update_ku_ke_cooefficients();
+        pid
+    }
+    fn update_ku_ke_cooefficients(&mut self) {
+        let a0 = 1.0 + self.N * self.Ts;
+        let a1 = -(2.0 + self.N * self.Ts);
+        let a2 = 1.0;
+        let b0 = self.Kp * (1.0 + self.N * self.Ts)
+            + self.Ki * self.Ts * (1.0 + self.N * self.Ts)
+            + self.Kd * self.N;
+        let b1 = -(self.Kp * (2.0 + self.N * self.Ts) + self.Ki * self.Ts + 2.0 * self.Kd * self.N);
+        let b2 = self.Kp + self.Kd * self.N;
+
+        self.ku = (a1 / a0, a2 / a0);
+        self.ke = (b0 / a0, b1 / a0, b2 / a0);
+    }
+    pub fn set_plant_output(&mut self, y: f64) {
+        self.y = y;
+    }
+    pub fn set_set_point(&mut self, r: f64) {
+        self.r = r;
+        // self.x = 0.0; // reset time when set point changes
+    }
+}
+
+impl Iterator for PIDController {
+    type Item = (f64, f64);
+    fn next(&mut self) -> Option<Self::Item> {
+        self.e.2 = self.e.1;
+        self.e.1 = self.e.0;
+        self.u.2 = self.u.1;
+        self.u.1 = self.u.0;
+        self.e.0 = self.r - self.y; // error = set point - plant_output
+        let ku1 = self.ku.0;
+        let ku2 = self.ku.1;
+        let controller_output = -ku1 * self.u.1 - ku2 * self.u.2
+            + self.ke.0 * self.e.0
+            + self.ke.1 * self.e.1
+            + self.ke.2 * self.e.2;
+        let point = (self.x, controller_output);
+        self.x += self.Ts;
+        self.u.0 = controller_output;
+        Some(point)
+    }
 }
 
 impl WidgetRef for &PIDController {
     fn render_ref(&self, area: Rect, buf: &mut Buffer) {
         let lines = vec![
             Line::from(Span::styled(
-                format!("P = {}", self.p),
+                format!("P = {}", self.Kp),
                 Style::default().add_modifier(Modifier::BOLD),
             )),
             Line::from(Span::styled(
-                format!("I = {}", self.i),
+                format!("I = {}", self.Ki),
                 Style::default().add_modifier(Modifier::BOLD),
             )),
             Line::from(Span::styled(
-                format!("D = {}", self.d),
+                format!("D = {}", self.Kd),
+                Style::default().add_modifier(Modifier::BOLD),
+            )),
+            Line::from(Span::styled(
+                format!("N = {}", self.N),
+                Style::default().add_modifier(Modifier::BOLD),
+            )),
+            Line::from(Span::styled(
+                format!("Ts = {}", self.Ts),
                 Style::default().add_modifier(Modifier::BOLD),
             )),
         ];
@@ -129,11 +216,12 @@ impl Iterator for SinSignal {
     }
 }
 
+#[allow(non_snake_case)]
 impl StepSignal {
-    const fn new(interval: f64, amplitude: f64) -> Self {
+    const fn new(Ts: f64, amplitude: f64) -> Self {
         Self {
             x: 0.0,
-            interval,
+            Ts,
             amplitude,
             amplitude_edit: None,
         }
@@ -144,7 +232,7 @@ impl Iterator for StepSignal {
     type Item = (f64, f64);
     fn next(&mut self) -> Option<Self::Item> {
         let point = (self.x, self.amplitude);
-        self.x += self.interval;
+        self.x += self.Ts;
         Some(point)
     }
 }
@@ -183,10 +271,11 @@ impl StatefulWidgetRef for &StepSignal {
 }
 
 impl FirstOrderSystem {
-    fn new(interval: f64, a: f64, b: f64, y_0: Option<f64>) -> Self {
+    #[allow(non_snake_case)]
+    fn new(Ts: f64, a: f64, b: f64, y_0: Option<f64>) -> Self {
         Self {
             x: 0.0,
-            interval,
+            Ts,
             a,
             b,
             u: 0.0,
@@ -203,7 +292,7 @@ impl Iterator for FirstOrderSystem {
     type Item = (f64, f64);
     fn next(&mut self) -> Option<Self::Item> {
         let point = (self.x, self.a * self.y_k + self.b * self.u);
-        self.x += self.interval;
+        self.x += self.Ts;
         self.y_k = point.1;
         Some(point)
     }
@@ -240,8 +329,10 @@ impl App {
         let samples_per_window = (window_size / sampling) as usize;
         let mut input = StepSignal::new(sampling, 15.0);
         let mut output = FirstOrderSystem::new(sampling, 0.95, 0.05, None);
+        let mut controller = PIDController::new(3.0, 1.9, 0.0, 10.0, sampling);
         let input_data = input.by_ref().take(0).collect::<Vec<(f64, f64)>>();
         let output_data = output.by_ref().take(0).collect::<Vec<(f64, f64)>>();
+        let controller_data = controller.by_ref().take(0).collect::<Vec<(f64, f64)>>();
         Self {
             input,
             input_data,
@@ -250,9 +341,10 @@ impl App {
             window: [0.0, window_size],
             samples_per_window,
             sampling,
-            controller: Default::default(),
+            controller,
             simulation_on: false,
             editing: Editing::None,
+            controller_data: controller_data,
         }
     }
 
@@ -337,12 +429,22 @@ impl App {
         }
         self.input_data.extend(self.input.by_ref().take(1));
 
+        self.controller
+            .set_set_point(self.input_data.last().map_or(0.0, |(_, y)| *y));
+        if self.controller_data.len() >= self.samples_per_window {
+            self.controller_data.drain(0..1);
+        }
+        self.controller_data.extend(self.controller.by_ref().take(1));
+
         self.output
-            .set_input(self.input_data.last().map_or(0.0, |(_, y)| *y));
+            .set_input(self.controller_data.last().map_or(0.0, |(_, y)| *y));
         if self.output_data.len() >= self.samples_per_window {
             self.output_data.drain(0..1);
         }
         self.output_data.extend(self.output.by_ref().take(1));
+
+        self.controller
+            .set_plant_output(self.output_data.last().map_or(0.0, |(_, y)| *y));
 
         if self.output_data.len() >= self.samples_per_window {
             self.window[0] += self.sampling;
